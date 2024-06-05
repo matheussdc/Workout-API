8f8bcc244e436faba412b3fe9b29102a8bd28525
Matheus Sá de Castro <mscastro30@gmail.com>
2024-06-05 11:16:45 -0300

    Schema e models base de atleta add

---
+++ atleta/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field, PositiveFloat
+from typing import Annotated
+
+
+class Atleta(BaseModel):
+    nome: Annotated[str, Field(description="Nome do atleta", examples=["Pedro", "João"], max_length=50)]
+    cpf: Annotated[str, Field(description="CPF do atleta", examples=["12345678901"], max_length=11)]
+    idade: Annotated[int, Field(description="Idade do atleta", examples=[24])]
+    peso: Annotated[PositiveFloat, Field(description="Peso do atleta", examples=[61.2])]
+    altura: Annotated[PositiveFloat, Field(description="Altura do atleta", examples=[1.72])]
+    sexo: Annotated[str, Field(description="Sexo do atleta", examples=['M'], max_length=1)]
