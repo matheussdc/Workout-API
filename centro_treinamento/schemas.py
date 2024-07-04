1e0aa7c57f41e2803456be7d41a3ee1347d8b152
Matheus Sá de Castro <mscastro30@gmail.com>
2024-07-04 20:57:46 -0300

    Rotas de Centro de Treinamento finalizadas

--- centro_treinamento/schemas.py
+++ centro_treinamento/schemas.py
@@ -1,9 +1,13 @@
 from typing import Annotated
-from pydantic import PositiveFloat, Field
+from pydantic import PositiveFloat, Field, UUID4
 from contrib.schemas import BaseSchema
 
 
-class CentroTreinamento(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[str, Field(description="Nome do Centro de Treinamento", examples=["CT Power"], max_length=20)]
     endereco: Annotated[str, Field(description="Endereço do Centro de Treinamento", examples=["Rua A, 123"], max_length=60)]
     proprietario: Annotated[str, Field(description="Nome do proprietário do Centro de Treinamento", example="Tom Cruise", max_length=30)]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]
